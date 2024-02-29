48068159310c43af5d3d80119db686a83a2de7e2
Andrey Zimenkov <gog13gogv@gmail.com>
2024-02-29 14:50:58 +0300

    feat: add common file to auth in k8s and get simple os.env with default values

---
+++ k8s/k8s-auth.go
@@ -0,0 +1,37 @@
+package k8s
+
+import (
+	"flag"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
+)
+
+var kubeconfig *string
+
+func init() {
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+}
+
+func GetKubeconfig() *string {
+	return kubeconfig
+}
+
+func inClusterAuth() *kubernetes.Clientset {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	return clientset
+}
